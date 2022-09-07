0e18d889943b0a4b72f6409920e056f27509ccff
Eric Biewener <eric.biewener0@walmartlabs.com>
2022-09-07 06:23:33 -0700

    more - 6:23 am - Sep 7

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.error('ERROR BOUNDARY CATCH', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 function MyApp({ Component, pageProps, ...rest }) {
   useEffect(() => {
-    console.info('::', 'mounting App', pageProps, rest)
+    console.info("::", "mounting App", pageProps, rest);
     return () => console.log("unmounting App");
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
 
 export default MyApp;
